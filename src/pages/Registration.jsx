098772912e777da6538d3cd3f7be981d77934feb
Sohel Rana <expartdesigns5@gmail.com>
2023-09-19 12:51:40 +0600

    registrtion pages design

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,21 +1,46 @@
 import React from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Grid } from "@mui/material";
+import logo from "../assets/logo.png"
 
 const Registration = () => {
     return (
-        <div>
-            <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-            />
-            <TextField
-                id="outlined-basic"
-                label="Password"
-                variant="outlined"
-            />
-            <Button variant="contained">Contained</Button>
-        </div>
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            item
+            xs={12}
+        >
+            <div className="box">
+                <div className="headdingTop">
+                    <img src={logo} alt="logo" />
+                    <h2>Get started with easily register</h2>
+                    <h4>Free register and you can enjoy it</h4>
+                </div>
+                <div className="inputBox">
+                    <TextField
+                        id="outlined-basic"
+                        label="Email Addres"
+                        variant="outlined"
+                        margin="dense"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Full name"
+                        variant="outlined"
+                        margin="dense"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Password"
+                        variant="outlined"
+                        margin="dense"
+                    />
+                    <Button variant="contained">Sing up</Button>
+                </div>
+            </div>
+        </Grid>
     );
 };
 
